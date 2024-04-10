bbdc1dd0b717067485e910dc32ee2bbba02a8e82
JenniferUgo <JENNIFERUGO.NN@GMAIL.COM>
2024-04-10 20:07:09 +0100

    Added jquery + html on JavaScript file

--- JavaScript/diaryApp/JS/diary.js
+++ JavaScript/diaryApp/JS/diary.js
@@ -1,20 +1,18 @@
 firebase.auth().onAuthStateChanged(async (user) => {
   if (user) {
-    document.getElementById("signout-button").addEventListener("click", () => {
-      firebase
-        .auth()
-        .signOut()
-        .then(() => {
+    document
+      .getElementById("signout-button")
+      .addEventListener("click", async () => {
+        try {
+          await firebase.auth().signOut();
           window.location.href = "signin.html";
-        })
-        .catch((error) => {
+        } catch (error) {
           console.log(error.code),
-            console.error("Signout error", error.message);
-        });
-    });
-    // document.getElementById("username").innerHTML = user.displayName;
+          console.error("Signout error", error.message);
+        }
+      });
 
-    loadDiaryEntries(user.uid);
+    //loadDiaryEntries(user.uid);
 
     document
       .getElementById("save-entry")
@@ -28,8 +26,7 @@ firebase.auth().onAuthStateChanged(async (user) => {
             content,
             userId: user.uid,
             username: user.displayName,
-            timestamp: firebase.firestore.FieldValue.serverTimestamp()
-                
+            timestamp: firebase.firestore.FieldValue.serverTimestamp(),
           });
 
           console.log("Diary entry saved!");
@@ -37,36 +34,72 @@ firebase.auth().onAuthStateChanged(async (user) => {
           console.log(error.code);
           console.error("Diary Error: ", error.message);
         }
+        window.location.reload();
       });
+
+    let loadDiaryEntries = await firebase
+      .firestore()
+      .collection("diaryEntries")
+      .get();
+
+    let diaryContent = "";
+    loadDiaryEntries.forEach((entryDoc) => {
+      let entryUserId = entryDoc.data().userId;
+      let userName = entryDoc.data().username;
+      let entryTitle = entryDoc.data().title;
+      let entryContent = entryDoc.data().content;
+
+      let userId = user.uid;
+      console.log(userId);
+      console.log(entryUserId);
+      if (userId == entryUserId) {
+        diaryContent += '<div class="diaryEntry">';
+        // diaryContent += "<p>" + entryUserId + "</p>";
+        diaryContent += "<p>" + userName + "</p>";
+        diaryContent += "<p>" + entryTitle + "</p>";
+        diaryContent += "<p>" + entryContent + "</p>";
+
+        diaryContent += "</div>";
+
+        console.log(entryUserId);
+        console.log(userName)
+        console.log(entryTitle);
+        console.log(entryContent);
+      } else {
+        console.log("Not your account");
+      }
+    });
+
+    $("#diary-list").append(diaryContent);
   } else {
     window.location.href = "signin.html";
   }
 });
 
-async function loadDiaryEntries(userId) {
-  // const userId = user.uid;
-  const querySnapshot = await firebase
-    .firestore()
-    .collection("diaryEntries")
-    .where("userId", "==", userId)
-    .orderBy("timestamp", "desc")
-    .get();
+// async function loadDiaryEntries(userId) {
+//   // const userId = user.uid;
+//   const querySnapshot = await firebase
+//     .firestore()
+//     .collection("diaryEntries")
+//     .where("userId", "==", userId)
+//     .orderBy("timestamp", "desc")
+//     .get();
 
-  const diaryList = document.getElementById("diary-list");
-  diaryList.innerHTML = "";
+//   const diaryList = document.getElementById("diary-list");
+//   diaryList.innerHTML = "";
 
-  querySnapshot.forEach((doc) => {
-    const data = doc.data();
+//   querySnapshot.forEach((doc) => {
+//     const data = doc.data();
 
-    const listItem = document.createElement("li");
-    const titleElement = document.createElement("h3");
-    const contentElement = document.createElement("p");
+//     const listItem = document.createElement("li");
+//     const titleElement = document.createElement("h3");
+//     const contentElement = document.createElement("p");
 
-    titleElement.textContent = data.title;
-    contentElement.textContent = data.content;
+//     titleElement.textContent = data.title;
+//     contentElement.textContent = data.content;
 
-    listItem.appendChild(titleElement);
-    listItem.appendChild(contentElement);
-    diaryList.appendChild(listItem);
-  });
-}
+//     listItem.appendChild(titleElement);
+//     listItem.appendChild(contentElement);
+//     diaryList.appendChild(listItem);
+//   });
+// }
